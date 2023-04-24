25e3c3c4ab207582867a1032f3981b5e832571c4
WanrongWu <71167314+WanrongWu@users.noreply.github.com>
2023-04-24 14:49:16 -0500

    Add files via upload

--- game-ui/layout/js/invt.js
+++ game-ui/layout/js/invt.js
@@ -3,12 +3,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    // var contentbox = this.nextElementSibling;
-    // if (contentbox.style.display === "block") {
-    //   contentbox.style.display = "none";
-    // } else {
-    //   contentbox.style.display = "block";
-    // }
+    this.classList.toggle("active").toggle("slide");
   });
 }
+
