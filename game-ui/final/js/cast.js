f460965a91fbadb8929e9dec9e3ecaf8ad2cb438
WanrongWu <71167314+WanrongWu@users.noreply.github.com>
2023-04-26 15:37:33 -0500

    Delete game-ui/final directory

--- game-ui/final/js/cast.js
+++
@@ -1,9 +0,0 @@
-  /* Toggle Animations */
-  const jstoggle = document.getElementById('stop');
-  jstoggle.addEventListener('click', () => {
-    const animations = document.querySelectorAll('[rbar-rolling');
-    animations.forEach(animation => {
-      const running = animation.style.animationPlayState || 'running';
-      animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
-    })
-  });
